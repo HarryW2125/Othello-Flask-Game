cfcea46284a42d8a2f93a38eb250ebb454c73011
HarryW2125 <hjw235@exeter.ac.uk>
2025-10-05 16:49:02 +0100

    Update components.py

--- components.py
+++ components.py
@@ -1,24 +1,37 @@
 
 #defines function that initialises the board, size is set to 8 by default
 def initialise_board(size=8):
+
+    #sets board array to empty
     board=[]
+
+    #nested loop to create a board thats size x size
     for i in range (size):
+        #sets spaces array to empty for each row
         spaces=[]
+
         for j in range (size):
+            #initially sets every space to none
             spaces.append("-None")
-        
+
+        #adds spaces array to board array
         board.append(spaces)
 
+    
+    #creates centre variables that represent the positions of the central spaces
+    #centre variables change depending on size of the grid
     centre1=int(size/2)-1
     centre2=int(size/2)
+    #initialises 4 central spaces to their corresponding colours
     board[centre1][centre1]="Light"
     board[centre2][centre1]="-Dark"
     board[centre1][centre2]="-Dark"
     board[centre2][centre2]="Light"
+
+    #returns board array
     return(board)
 
 board= initialise_board()
-for row in board:
-    print(row)      
+    
 
     
