372545b0ec233af6b03cc976dce21b951842f5a0
HarryW2125 <hjw235@exeter.ac.uk>
2025-10-05 16:35:37 +0100

    Update components.py

--- components.py
+++ components.py
@@ -1,2 +1,24 @@
+
+#defines function that initialises the board, size is set to 8 by default
 def initialise_board(size=8):
+    board=[]
+    for i in range (size):
+        spaces=[]
+        for j in range (size):
+            spaces.append("-None")
+        
+        board.append(spaces)
+
+    centre1=int(size/2)-1
+    centre2=int(size/2)
+    board[centre1][centre1]="Light"
+    board[centre2][centre1]="-Dark"
+    board[centre1][centre2]="-Dark"
+    board[centre2][centre2]="Light"
+    return(board)
+
+board= initialise_board()
+for row in board:
+    print(row)      
+
     
