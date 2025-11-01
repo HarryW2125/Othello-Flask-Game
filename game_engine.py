0d5e9f2167dd2c8f8ec2a0281f215720495e365d
HarryW2125 <hjw235@exeter.ac.uk>
2025-11-01 12:41:32 +0000

    Update game_engine.py

--- game_engine.py
+++ game_engine.py
@@ -1,6 +1,12 @@
 def cli_coords_input():
-    x_coord=int(input("Enter the x co-ordinate"))
-    y_coord=int(input("Enter the y co-ordinate"))
+    valid = False
+    while valid == False:
+        try:
+            x_coord=int(input("Enter the x co-ordinate"))
+            y_coord=int(input("Enter the y co-ordinate"))
+
+        except ValueError:
+            continue
 
 
 
