cdc6715d332da87c6b8f0bf991bda21ce1a4b3a7
HarryW2125 <hjw235@exeter.ac.uk>
2025-11-01 12:44:31 +0000

    Update game_engine.py

--- game_engine.py
+++ game_engine.py
@@ -6,8 +6,17 @@ def cli_coords_input():
             y_coord=int(input("Enter the y co-ordinate"))
 
         except ValueError:
+            print("inputs must be integers")
             continue
 
+        if x_coord <= 8 and y_coord <= 8:
+            valid = True
+        else:
+            print("inputs must be in the 8x8 grid")
+            continue
+    
+    coord=tuple(x_coord,y_coord)
+    return coord
 
 
 cli_coords_input()
