b7cf45f09474b8c2e14402cffe96cf9eedd202b4
HarryW2125 <hjw235@exeter.ac.uk>
2025-11-19 16:33:00 +0000

    Update flask_game_engine.py

--- flask_game_engine.py
+++ flask_game_engine.py
@@ -37,41 +37,44 @@ def process_move():
         #gets x and y variables from html template
         x = int(request.args['x']) -1
         y = int(request.args['y']) -1
+
         # if space is taken move isn't valid
         if board[y][x] != "-None":
             return jsonify( { "status": "fail", "player": current_player, "message": "Tile already placed here"})
 
-        is_valid, direction = components.legal_move(current_player, (x,y), board)
+        is_valid, directions = components.legal_move(current_player, (x,y), board)
         if is_valid == True:
-                flip_arr = []
                 #flank count initially set to 1 as initial tile is already flipped before entering the loop
                 flank_count =1
                 #changes initial tile to current player
-                board[y][x] = current_player  
-                #moves x and y one step in the right direction
-                x += direction[0]
-                y += direction[1]
+                board[y][x] = current_player
 
-                #runs whilst the tile is on the board
-                while 0<= x <=7 and 0<= y <=7:
-                    #if current tile is empty breaks out of the loop
-                    if board[y][x] == "-None":
-                        break
+                for direction in directions: 
+                    flip_arr = []
+                    #moves x and y one step in the right direction
+                    current_x = x + direction[0]
+                    current_y = y + direction[1]
+
+                    #runs whilst the tile is on the board
+                    while 0<= current_x <=7 and 0<= current_y <=7:
+                        #if current tile is empty breaks out of the loop
+                        if board[current_y][current_x] == "-None":
+                            break
                     
-                    # if the tile is the colour of the current player, flip all of the tiles in flip_arr to current players colour
-                    elif board[y][x] == current_player:
+                        # if the tile is the colour of the current player, flip all of the tiles in flip_arr to current players colour
+                        elif board[current_y][current_x] == current_player:
                         
-                        for x,y in flip_arr:
-                            flank_count+=1
-                            board[y][x] = current_player
-                        break
+                            for current_x,current_y in flip_arr:
+                                flank_count+=1
+                                board[current_y][current_x] = current_player
+                            break
                     
-                    # if the tile is the opposite colour, increment replace count and add the coord to the flip arr
-                    else:
-                        flip_arr.append((x,y))
-                        #moves x and y one step in the right direction
-                        x += direction[0]
-                        y += direction[1]
+                        # if the tile is the opposite colour, increment replace count and add the coord to the flip arr
+                        else:
+                            flip_arr.append((current_x,current_y))
+                         #moves x and y one step in the right direction
+                            current_x += direction[0]
+                            current_y += direction[1]
 
                 current_player = player_swap(current_player)
                 
