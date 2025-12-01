8a39590626bcb0aeaf1164dfca067224da5ec07d
HarryW2125 <hjw235@exeter.ac.uk>
2025-12-01 20:31:02 +0000

    Update components.py

--- components.py
+++ components.py
@@ -4,30 +4,36 @@ def initialise_board(size=8):
     '''Function that initialises the board, size is set to 8 by default.'''
     #sets board array to empty
     board=[]
+    
+    #error handling
+    try:
+        #nested loop to create a board thats size x size
+        for i in range(size):
+            #sets spaces array to empty for each row
+            spaces=[]
 
-    #nested loop to create a board thats size x size
-    for i in range(size):
-        #sets spaces array to empty for each row
-        spaces=[]
+            for j in range(size):
+                #initially sets every space to none
+                spaces.append("-None")
+            #adds spaces array to board array
+            board.append(spaces)
 
-        for j in range(size):
-            #initially sets every space to none
-            spaces.append("-None")
-        #adds spaces array to board array
-        board.append(spaces)
+        #creates centre variables that represent the positions of the central spaces
+        #centre variables change depending on size of the grid
+        centre1=int(size/2)-1
+        centre2=int(size/2)
+        #initialises 4 central spaces to their corresponding colours
+        board[centre1][centre1]="Light"
+        board[centre2][centre1]="-Dark"
+        board[centre1][centre2]="-Dark"
+        board[centre2][centre2]="Light"
 
-    #creates centre variables that represent the positions of the central spaces
-    #centre variables change depending on size of the grid
-    centre1=int(size/2)-1
-    centre2=int(size/2)
-    #initialises 4 central spaces to their corresponding colours
-    board[centre1][centre1]="Light"
-    board[centre2][centre1]="-Dark"
-    board[centre1][centre2]="-Dark"
-    board[centre2][centre2]="Light"
-
-    #returns board array
-    return board
+        #returns board array
+        return board
+    
+    #catches errors
+    except:
+        return None
 
 
 #print normal characters
